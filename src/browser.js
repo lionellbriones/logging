56143e03f95173364f80d6344e6549678dc142c8
chaitanyapotti <chaitanya.potti@gmail.com>
2020-10-24 11:31:38 +0800

    Add support for original console fns

--- src/browser.js
+++ src/browser.js
@@ -1,14 +1,14 @@
 // super basic browser support just to avoid breaking universal builds.
 // title and log level are not included in the output at this time.
 function createLogger(/* title */) {
-    function logger() {
-        console.log.apply(console, arguments);
+    function logger(key) {
+        console[key].apply(console, arguments);
     }
     return {
-        info: logger,
-        warn: logger,
-        error: logger,
-        debug: logger
+        info: logger("info"),
+        warn: logger("warn"),
+        error: logger("error"),
+        debug: logger("debug")
     };
 }
 
